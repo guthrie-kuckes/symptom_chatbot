f37a44f57f71e60af433ecd7880cbe5adcb94fb1
jimenezh <jimenezh@bu.edu>
2020-05-28 14:27:41 -0400

    Firebase config

--- visualization/src/firebase/init.js
+++ visualization/src/firebase/init.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth"
 import "firebase/firestore";
 
-var firebaseConfig = {
+const config = {
     apiKey: "AIzaSyBoPENiji-slQ3gjdjEY1WRKOfNgatSpMU",
     authDomain: "symptom-chatbot.firebaseapp.com",
     databaseURL: "https://symptom-chatbot.firebaseio.com",
@@ -12,10 +12,10 @@ var firebaseConfig = {
     appId: "1:631219149444:web:a52060cf60445e122380d1",
     measurementId: "G-M1BCXH3QKV"
 };
-// Initialize Firebase
+
 const app = firebase.initializeApp(config);
 const auth = firebase.auth();
 const db = app.firestore();
 
 export default app;
-export { auth, db, authUI, authUIConfig };
+export { auth, db};
