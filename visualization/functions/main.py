5bb29a55b8abb648bdbf7c80d1d6b92ff59379b5
fgpacini <fgpacini@yahoo.com>
2020-06-01 12:09:14 -0400

    cloud function

--- visualization/functions/main.py
+++ visualization/functions/main.py
@@ -31,3 +31,44 @@ def hello_http(request):
     else:
         name = 'World'
     return 'Hello {}!'.format(escape(name))
+
+def sort_data(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using make_response
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    try:
+        from google.cloud import firestore
+        _request = request.get_json(silent=True)
+        if not _request:
+            _request = request.args
+        if not _request: 
+            return None
+        db = firestore.Client()
+        q = db.collection(u'users')
+        for elem in _request.keys():
+            if not _request[elem] == '':
+                q = q.where(elem, u'==', _request[elem])
+        docs = q.stream()
+        l = []
+        for doc in docs:
+            next = doc.to_dict()
+            for elem in _request.keys():
+                next.pop(elem)
+            l.append(next)
+        return l
+    except ImportError:
+        print ("NO module found")
+
+    # if request_json and 'name' in request_json:
+    #     name = request_json['name']
+    # elif request_args and 'name' in request_args:
+    #     name = request_args['name']
+    # else:
+    #     name = 'World'
+    # return 'Hello {}!'.format(escape(name))
